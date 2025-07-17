1f002a4bfc381b2f4d5e6b5aea138a309f99b9ab
root <root@ip-172-31-47-51.eu-north-1.compute.internal>
2025-07-17 09:09:03 +0000

    docker deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Code deployment in Docker test 3');
+  res.send('Code deployment in Docker test 4');
 });
 
 app.listen(PORT, () => {
