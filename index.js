3abd53030cf28ab1fdc4e5dee6581281db750656
root <root@ip-172-31-40-183.eu-north-1.compute.internal>
2025-07-23 11:16:02 +0000

    error

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const PORT = 4000;
 
 app.get('/', (req, res) => {
+app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('deploy test using jenkins and webhook');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
 });
 
